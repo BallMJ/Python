d38a4b597440d7b07c2b13b60e94b89a2050808a
BallMJ <matthewball4@btinternet.com>
2016-02-19 19:23:49 +0000

    Interactive Web Scraper
    Interactive and ready to use URL Web Scraper, for grabbing URL's on
    specific webpages

---
+++ Web Scraping/urlwebscraper.py
@@ -0,0 +1,28 @@
+""" Webscraper which takes URL input and scrapes all links
+within the webpage, then writes them to a text file for using/editing"""
+
+import urllib
+import sys
+from bs4 import BeautifulSoup
+
+# Creates and opens txt file to write wbescrape output to
+x = open("links.txt", "w")
+
+url = raw_input("Enter URL for link scraping: ")
+
+# Checks is url contains the hypertext transfer protocol, and adds it if it doesn't 
+if url[0] != "http://":
+    url="http://"+url
+
+# Assigning variables for scraping
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+links = soup.find_all('a')
+
+# Running through all urls in webpage, and writing each one to txt file
+for i in links:
+    if 'href' in str(i):
+        print >>x, i
+
+print "Web Scraping complete! Output can be found in links.txt"
+    
